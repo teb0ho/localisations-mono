72e094248dd44da394063d4c8bc50cc1b673ba7f
Teboho Mphure <tebohomphure@gmail.com>
2023-10-07 12:52:40 +0200

    add the ui project

---
+++ Localisations-ui/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <div className="w-96">
+        <h1 className="text-3xl text-center font-bold">
+          Welcome to Localisation UI, your perfect app for managing your strings
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
